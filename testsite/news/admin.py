f2e43ec74b0e981a33acbfb5cec20564624f4559
Mazaxakasf <Arty_h41@mail.ru>
2022-10-05 00:23:37 +0300

    настраиваем админку 2

--- testsite/news/admin.py
+++ testsite/news/admin.py
@@ -5,6 +5,8 @@ from .models import News
 # настройки админки
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published') # поля для отображения в админке
+    list_display_links = ('id', 'title') #наименования которые будут ссылкой в админ
+    search_fields = ('title', 'content') #поиск по полям
 
 
 # регистрируем приложуху в админке и настройки
