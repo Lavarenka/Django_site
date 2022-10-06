d84189c92ea3fe3e25a1b2a719ad4edf0bdee2df
Mazaxakasf <Arty_h41@mail.ru>
2022-10-07 00:09:28 +0300

    connecting list-group category

--- testsite/news/urls.py
+++ testsite/news/urls.py
@@ -3,4 +3,5 @@ from .views import *
 # подключаем страницчки
 urlpatterns = [
     path('', index),
+    path('category/<int:category_id>', get_category),
 ]
--- testsite/news/views.py
+++ testsite/news/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': 'Список новостей'
+        'title': 'Список новостей',
+        'categories': categories,
     }
     # 1 параметр request, 2 параметр название шаблона, 3 параметр контекст
     return render(request, 'news/index.html', context)
 
+def get_category(request, category_id):
+    news = News.objects.filter(category_id)
