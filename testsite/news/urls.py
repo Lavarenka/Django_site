497e35d4c0160a308c73eb761e059e517331703b
Mazaxakasf <Arty_h41@mail.ru>
2022-10-14 18:58:42 +0300

    ООП

--- testsite/news/urls.py
+++ testsite/news/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from .views import *
 # подключаем страницчки
 urlpatterns = [
-    path('', index, name='home'),
+    # path('', index, name='home'),
+    path('', HomeNews.as_view(), name='home'), # вызываем класс из viev
     path('category/<int:category_id>', get_category, name='category'),
     path('news/<int:news_id>', view_news, name='view_news'),
     path('news/add_news', add_news, name='add_news'),
